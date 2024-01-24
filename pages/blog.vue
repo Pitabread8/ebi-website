af9d4184522a78eb66463ad1a9dc312a4eca6239
Sita <sitavemuri8@gmail.com>
2024-01-24 09:43:23 -0800

    Implement blog

--- pages/blog.vue
+++ pages/blog.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <NavBar />
-    <NuxtPage />
+    <p>Blog Home</p>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <NavBar />
-    <NuxtPage />
+    <p>Home</p>
   </div>
 </template>
